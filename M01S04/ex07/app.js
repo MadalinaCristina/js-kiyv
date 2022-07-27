d2fcef12c4539a6e1ab85edc9e8431b3f72d0913
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-07-27 20:33:12 +0300

    833

--- M01S04/ex07/app.js
+++ M01S04/ex07/app.js
@@ -21,13 +21,15 @@ var friends = [
   },
 ];
 
+var friendsCount = friends.length;
+
 console.warn(
   `
     Folosind o bucla for afiseaza proprietatea
     surname a tuturor obiectelor din arrayul friends.
   `,
 );
-for (var i = 0; i < friends.length; i++) {
+for (var i = 0; i < friendsCount; i++) {
   console.log(friends[i].surname);
 }
 
@@ -38,3 +40,34 @@ console.warn(
     bucla cu ajutorul keywordului break si afiseaza propozitia: “M-am oprit la Stevenson Steven.”.
   `,
 );
+
+for (var i = 0; i < friendsCount; i++) {
+  var friend = friends[i];
+  var fullName = friend.surname + ' ' + friend.name;
+
+  if (friend.name === 'Steven') {
+    console.log('M-am oprit la ' + fullName + '.');
+
+    break;
+  }
+
+  console.log(fullName);
+}
+
+console.warn(
+  `
+    Folosind keywordul continue, afiseaza numele complet inversat al prietenilor,
+    doar daca numarul de caractere total al numelor lor este mai mare de 13
+    sub forma de propozitii “Prenume Nume are mai mult de 13 caractere.”.
+  `,
+);
+for (var i = 0; i < friendsCount; i++) {
+  var friend = friends[i];
+  var fullName = friend.surname + ' ' + friend.name;
+
+  if (fullName.replaceAll(' ', '').length <= 13) {
+    continue;
+  }
+
+  console.log(fullName + ' are mai mult de 13 caractere.');
+}
