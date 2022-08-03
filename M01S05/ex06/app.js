3ca6e825fbac5f1ccf1468ae7092cf7f9fe22b3b
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-08-03 21:00:39 +0300

    900

--- M01S05/ex06/app.js
+++ M01S05/ex06/app.js
@@ -15,3 +15,35 @@ function accessor(methodSuffix) {
 }
 
 console.log(accessor('Name'));
+
+console.warn(
+  `
+    Folosind accesorul salveaza numele mic al
+    persoanei intr-o variabila si foloseste-l
+    in propozitia: “Eu sunt xxx.”
+  `,
+);
+const firstName = accessor('Name');
+console.log(`Eu sunt ${firstName}.`);
+
+console.warn(
+  `
+    Afiseaza anul de nastere al persoanei
+    folosind anul curent.
+  `,
+);
+const currentYear = new Date().getFullYear();
+const birthYear = currentYear - accessor('Age');
+console.log(birthYear.toString());
+
+console.warn(
+  `
+    Afiseaza propozitia
+    “Ma numesc xxx yyy, am aa ani si m-am nascut in anul aaaa.”
+  `,
+);
+console.log(
+  `Ma numesc ${accessor('Name')}, am ${accessor(
+    'Age',
+  )} ani si m-am nascut in anul ${birthYear}.`,
+);
