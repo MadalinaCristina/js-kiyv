f91381b39480256ddc20fb81520581458c13bb5c
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-08-22 19:45:19 +0300

    745

--- M02S02/ex01/app.js
+++ M02S02/ex01/app.js
@@ -3,4 +3,59 @@
 // p.id = 'myParagraph';
 // p.classList.add('clase', 'de', 'css');
 
-// document.body.appendChild(p);
+// document.body.append(p);
+
+const $p = $('<p>', {
+  text: 'Textul care trebuie sa apara in paragraf',
+  id: 'myParagraph',
+  class: 'clase de css',
+});
+const $msg = $('<p>', {
+  id: 'message',
+  text: 'Parola nu trebuie sa fie mai mica de 5 caractere',
+});
+
+const $container = $('.container');
+$p.appendTo($container);
+
+$container.append($msg);
+
+setTimeout(function () {
+  $msg.addClass('error');
+}, 5000);
+
+$('<span>', {
+  id: 'example',
+  text: 'ParolaMea',
+}).appendTo($msg);
+
+$('<h2>', {
+  text: 'Mesaje',
+}).prependTo($container);
+
+// $('<div>', {
+//   class: 'navigation',
+//   text: 'x',
+// }).after($container);
+// ^ wrong
+
+$container.after(
+  $('<div>', {
+    class: 'navigation',
+  }),
+);
+
+$('.navigation').append(
+  $('<a>', {
+    text: 'Primul Link',
+    href: '/',
+    title: 'Test',
+    class: 'nav-link',
+  }),
+);
+
+$('.navigation .nav-link').before(
+  $('<h2>', {
+    text: 'Navigatie',
+  }),
+);
